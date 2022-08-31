15784001343ad2e126869d4b81ffd41c25eb682a
abhi00 <abhijit.pandit808@gmail.com>
2022-08-30 17:11:18 -0700

    added todos

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame as pyg
 from universe import Universe
 from data import microSolarSystem
 
+#TODO Add collisions
+#TODO Add solar system dataset
+#TODO Add unit converter
+#TODO Add controllable spacecraft
+#TODO Add orbit prediction
+
 if __name__ == '__main__':
     pyg.init()
     x = Universe(800, 800, microSolarSystem)

effe3b52558fea32bde75eea4e6c9dcd5eff08dd
abhi00 <abhijit.pandit808@gmail.com>
2022-09-04 01:46:07 -0700

    working GLAD-GLFW installation

--- main.py
+++
@@ -1,9 +0,0 @@
-import pygame as pyg
-import constants
-from universe import Universe
-from data import microSolarSystem, microExplorer
-
-if __name__ == '__main__':
-    pyg.init()
-    x = Universe(800, 800, celestialObjects=microSolarSystem, explorer=microExplorer)
-    x.simulate(0.4 * constants.units.T, 10000 * constants.units.T)
